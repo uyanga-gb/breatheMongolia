537f808f21e3122a40bf967cec2df114eef61215
uyanga-gb <uyanga.gb@gmail.com>
2018-07-29 10:30:50 -0700

    initial environment setup for react and webpack app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './components/app'
+
+const app = () => {
+  return <App />
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
